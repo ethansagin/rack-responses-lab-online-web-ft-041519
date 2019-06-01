211b47528b20b4fe80606ec405c344b5d245af32
Ethan Sagin <ethansagin@gmail.com>
2019-06-01 18:35:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if current_time.hour < 12
       resp.write "Good Morning!"
-    elsif current_time >= 12
+    elsif current_time.hour >= 12
       resp.write "Good Afternoon!"
     end
     
